7d57d414cfbf68c4d0392517b8b3909cf1e6d358
pajosego <144797657+pajosego@users.noreply.github.com>
2025-07-18 12:55:11 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const CHAT_ID = process.env.CHAT_ID;
 
 async function start() {
   await monitor(CHAT_ID);
-  setInterval(() => monitor(CHAT_ID), 10 * 60 * 1000);
+  setInterval(() => monitor(CHAT_ID), 60 * 60 * 1000);
   // Evita encerramento automático no Railway
   process.stdin.resume();
 }
